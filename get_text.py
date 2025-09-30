7fa289260b9a5d480cd15548e59218d3959f9f4c
ankitsharma-tech <ankitsharma224589@gmail.com>
2025-09-30 21:31:45 +0500

    Update

--- get_text.py
+++
@@ -1,7 +0,0 @@
-from scraper import scrape
-
-
-def get_text(url):
-    soup = scrape(url)
-    paragraphs = soup.find_all('p')
-    return '\n'.join([paragraph.get_text().strip() for paragraph in paragraphs])
--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('#instagram_form').on('submit', function(event) {
-        $('#loadingModal').modal('show');
-    });
-});
